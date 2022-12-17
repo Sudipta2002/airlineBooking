3c447cdee9e1f8d75ef9cc9fed2b98ef7092503a
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-12-17 09:51:50 +0530

    body-parser setup

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const { PORT } = require('./config/serverConfig');
 // const dotenv = require('dotenv');
 // dotenv.config();
+const bodyParser = require('body-parser')
 
 const setupAndStartServer = async() => {
     /// create a express object
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, () => {
         console.log(`Server running at port number ${PORT}`);
