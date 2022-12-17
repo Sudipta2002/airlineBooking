c2f6b7f0ade7f3b702c56c03fd0915923cd4b7b0
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-12-17 09:37:36 +0530

    UPDATED CONFIG

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
+const { PORT } = require('./config/serverConfig');
+// const dotenv = require('dotenv');
+// dotenv.config();
+
 const setupAndStartServer = async() => {
     /// create a express object
     const app = express();
 
-    app.listen(process.env.PORT, () => {
-        console.log(`Server running at port number ${process.env.PORT}`);
+    app.listen(PORT, () => {
+        console.log(`Server running at port number ${PORT}`);
     });
 }
 setupAndStartServer();
