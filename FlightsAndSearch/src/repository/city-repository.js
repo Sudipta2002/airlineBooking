89de47de5103da64ab1174db7b0387a23e8d83c4
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-12-19 15:36:00 +0530

    minor fixes

--- FlightsAndSearch/src/repository/city-repository.js
+++ FlightsAndSearch/src/repository/city-repository.js
@@ -14,7 +14,7 @@ class CityRepository {
 
     async deleteCity(cityId) {
         try {
-            await City.destroy({
+            await City.destroy({ 
                 where: {
                     id: cityId
                 }
@@ -25,19 +25,26 @@ class CityRepository {
             throw { error };
         }
     }
+
     async updateCity(cityId,data){
         try {
-            const city = await City.update(data,{
-                where:{
-                    id:cityId
-                }
-            });
+            // const city = await City.update(data,{
+            //     where:{
+            //         id:cityId
+            //     }
+            // });
+            const city = await City.findByPk(cityId);
+            city.name=data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw{error};
         }
     }
+
+
+
     async getCity(cityId){
         try {
             const city = await City.findByPk(cityId);
