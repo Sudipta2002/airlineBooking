270c018e9b84d29a7f10ebd85e05635b8ab5d66a
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-12-19 15:13:35 +0530

    added routers to complete create api

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 
 const CityRepository = require('./repository/city-repository');
 
-
+const ApiRoutes = require('./routes/index');
 
 
 
@@ -16,6 +16,8 @@ const setupAndStartServer = async() => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/api', ApiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server running at port number ${PORT}`);
